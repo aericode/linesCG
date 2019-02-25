abcb5c8df5200cdb200980621f5e9ff2274bda95
Eric Andrade <vloxflox@gmail.com>
2019-02-25 20:29:48 -0300

    adicionando cin e cout

--- main.cpp
+++ main.cpp
@@ -2,9 +2,25 @@
 #include "point.h"
 #include "canvas.h"
 
+void basicPoint(Point& insert){
+	int x;
+	int y;
+	std::cout<< "insert point's X" <<std::endl;
+	std::cin >> x;
+	std::cout<< "insert point's Y" <<std::endl;
+	std::cin >> y;
+
+	insert.setX(x);
+	insert.setY(y);
+
+}
+
 int main(){
 	Point starter(0,10);
 	Point final(99,20);
+
+	basicPoint(starter);
+	basicPoint(final);
 
 	Canvas canvas(100,100);
 	canvas.drawLineBetweenPoints(starter, final);
